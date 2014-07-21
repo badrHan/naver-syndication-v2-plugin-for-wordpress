c76ac5eee644bf316518f07ed68bdb4680b80b2e
webmaster <webmaster@badrHan-book>
2014-07-21 13:56:26 +0900

    0.7.2 tag

--- badr-syndication.php
+++ badr-syndication.php
@@ -1,8 +1,8 @@
 <?php
 /* Plugin name: 네이버 신디케이션 V2
-   Plugin URI:http://note.badr.kr/category/syndication/
-   Author: changhoon Han
-   Author URI: http://note.badr.kr
+   Plugin URI: http://badr.kr/?p=1152
+   Author: badr Han
+   Author URI: http://badr.kr
    Version: 0.7
    Description: 네이버 신디케이션 문서란, 웹 사이트의 콘텐츠를 네이버 웹 서비스에 전달할 수 있도록 정해진 형식에 맞춰 작성한 문서입니다
    Max WP Version: 3.9
